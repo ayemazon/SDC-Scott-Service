eb3995f5191624c2fa4c17b40cdc015c8f5e69fe
deadcode <josephsonsjoseph@gmail.com>
2019-08-13 16:31:33 -0700

    "Finished writing basic CRUD functions"

--- database-mysql/crud_test.js
+++ database-mysql/crud_test.js
@@ -1,9 +1,27 @@
 const crud = require('./index.js');
 
-crud.insertIntoTable('item', {name: 'Scott Peterson'}, (err, results) => {
-  if (err) {
-    console.log('INSERT CRUD ERROR: ', err);
-  } else {
-    console.log('RESULTS: ' ,results)
-  }
-})
+// TESTS FOR CRUD FUNCTIONS //
+
+// crud.createRecord('item', {name: 'Scott Peterson'}, (err, results) => {
+//   if (err) {
+//     console.log('INSERT CRUD ERROR: ', err);
+//   } else {
+//     console.log('RESULTS: ' ,results)
+//   }
+// })
+
+// crud.updateRecord('vendor', {name: 'Dork Jorkins'}, 100, (err, results) => {
+//   if (err) {
+//     console.log('UPDATE CRUD ERROR: ', err);
+//   } else {
+//     console.log('RESULTS: ' ,results)
+//   }
+// })
+
+// crud.deleteRecord('item', 102, (err, results) => {
+//   if (err) {
+//     console.log('DELETE CRUD ERROR: ', err);
+//   } else {
+//     console.log('RESULTS: ' ,results)
+//   }
+// })
--- database-mysql/index.js
+++ database-mysql/index.js
@@ -10,9 +10,8 @@ connection.connect((err) => {
 });
 
 //------------ CRUD ----------//
-const insertIntoTable = function (tableName, data, cb) {
+const createRecord = function (tableName, data, cb) {
   var queryString = `INSERT INTO ${tableName} SET ?`;
-  console.log(queryString);
   connection.query(queryString, [data], (err, dbRes) => {
     if (err) {
       cb(err, null);
@@ -22,23 +21,44 @@ const insertIntoTable = function (tableName, data, cb) {
   });
 };
 
+const updateRecord = function (tableName, data, id, cb) {
+  var queryString = `UPDATE ${tableName} SET ? WHERE id = ${id}`;
+  connection.query(queryString, [data], (err, dbRes) => {
+    if (err) {
+      cb(err, null);
+    } else {
+      cb(null, dbRes);
+    }
+  });
+}
+
+const deleteRecord = function (tableName, id, cb) {
+  var queryString = `DELETE FROM ${tableName} WHERE id = ?`;
+  console.log(queryString);
+  connection.query(queryString, [id], (err, dbRes) => {
+    if (err) {
+      cb(err, null);
+    } else {
+      cb(null, dbRes);
+    }
+  });
+}
+
 //----------------------------//
 
 const getProductDataById = function (id, cb) {
-  // var dbRes = {};
-  // dbRes.id = id;
 
   var queryString = `SELECT itemAvail.item_id, itemAvail.vendor_id, itemAvail.amz_holds_stock, itemAvail.quantity_available, itemAvail.price, 
   vendor.id, vendor.name, vendor.free_returns, vendor.ships_on_saturday, vendor.ships_on_sunday, vendor.ships_from_zipcode
   FROM item_availability AS itemAvail 
   INNER JOIN vendor
-  WHERE vendor.id = ${id} AND itemAvail.vendor_id = ${id}`;
+  WHERE itemAvail.item_id = ${id} AND vendor.id = itemAvail.vendor_id`;
 
   connection.query(queryString, (err, dbRes) => {
     if (err) {
       console.log('MYSQL select by item_id error ' + err);
       cb(err, null);
-    } else {
+    } else if (dbRes.length) {
       dbRes = dbRes[0];
       dbRes.gift_wrap_available = true;
       dbRes.user_zip = "78726";
@@ -56,16 +76,15 @@ const getProductDataById = function (id, cb) {
         dbRes.expected_shipping = "4-5 Days";
       }
       dbRes.free_delivery = ((dbRes.free_returns == 1) ? true : false);
-
-      cb(null, dbRes);
     }
-
-
+    cb(null, dbRes);
 
   });
 };
 
 module.exports = {
-  insertIntoTable: insertIntoTable,
+  createRecord: createRecord,
+  updateRecord: updateRecord,
+  deleteRecord: deleteRecord,
   getProductDataById: getProductDataById
 };
