550e50fb5d7f84e7124087508e64c8f0c01365ce
deadcode <josephsonsjoseph@gmail.com>
2019-08-14 19:36:46 -0700

    "Wrote data generation and seeded MySQL w/10M records"

--- database-mysql/crud_test.js
+++ database-mysql/crud_test.js
@@ -25,3 +25,9 @@ const crud = require('./index.js');
 //     console.log('RESULTS: ' ,results)
 //   }
 // })
+
+// SELECT itemAvail.item_id, itemAvail.vendor_id, itemAvail.amz_holds_stock, itemAvail.quantity_available, itemAvail.price, 
+//   vendor.id, vendor.name, vendor.free_returns, vendor.ships_on_saturday, vendor.ships_on_sunday, vendor.ships_from_zipcode
+//   FROM items_vendors AS itemAvail 
+//   INNER JOIN vendor
+//   WHERE itemAvail.item_id = 199999 AND vendor.id = itemAvail.vendor_id;
